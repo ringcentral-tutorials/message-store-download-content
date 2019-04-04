61fbed7ecab82b139bd7d59743d5afca3c468da3
PacoVu <paco_vu@yahoo.com>
2019-04-04 15:05:16 -0700

    Update index.js

--- index.js
+++ index.js
@@ -24,173 +24,19 @@ platform.login({
           read_message_store_message_content()
       });
 
-function read_message_store_sms_mms_limit(){
-    platform.get('/account/~/extension/~/message-store', {
-             dateFrom: '2018-01-01T00:00:00.000Z',
-             dateTo: '2018-12-31T23:59:59.999Z',
-             messageType: 'SMS'
-        })
-        .then(function (resp) {
-          var jsonObj = resp.json()
-          var count = jsonObj.records.length
-          var index = 0
-          var dir = "sms_mms_content/"
-          if(!fs.existsSync(dir)){
-            fs.mkdirSync(dir)
-          }
-          dir = "./" + dir
-          // Limit API call to ~40 calls per minute to avoid exceeding API rate limit.
-          var interval = setInterval(function() {
-            var record = jsonObj.records[index]
-            if (record.hasOwnProperty('attachments')){
-              async.each(record.attachments,
-                function(attachment, callback){
-                  var fileName = record.attachments[0].id
-                  if (attachment.type == "MmsAttachment"){
-                    var fileNameExt = attachment.contentType.split("/")
-                    fileName += "_mms_attachment." + fileNameExt[1]
-                  }else{
-                    fileName += "_mms_text.txt"
-                  }
-                  platform.get(attachment.uri)
-                      .then(function(res) {
-                          return res.response().buffer();
-                      })
-                      .then(function(buffer) {
-                          fs.writeFile(dir + fileName, buffer, function(){
-                            callback()
-                          })
-                      })
-                      .catch(function(e){
-                          console.log(e)
-                          callback()
-                      })
-                })
-            }
-            index++
-            if (index >= count){
-                clearInterval(interval);
-            }
-          }, 2000);
-        });
-}
-
-function read_message_store_fax_limit(){
-    platform.get('/account/~/extension/~/message-store', {
-          dateFrom: '2018-01-01T00:00:00.000Z',
-          dateTo: '2018-12-31T23:59:59.999Z',
-          messageType: 'Fax'
-        })
-        .then(function (resp) {
-            var jsonObj = resp.json()
-            var count = jsonObj.records.length
-            var index = 0
-            var dir = "fax_content/"
-            if(!fs.existsSync(dir)){
-              fs.mkdirSync(dir)
-            }
-            dir = "./" + dir
-            // Limit API call to ~40 calls per minute to avoid exceeding API rate limit.
-            var interval = setInterval(function() {
-              var record = jsonObj.records[index]
-                if (record.hasOwnProperty('attachments')){
-                  async.each(record.attachments,
-                    function(attachment, callback){
-                      var fileName = attachment.id + "_fax_attachment"
-                      var fileNameExt = attachment.contentType.split("/")
-                      fileName += "." + fileNameExt[1]
-                      platform.get(attachment.uri)
-                          .then(function(res) {
-                              return res.response().buffer();
-                          })
-                          .then(function(buffer) {
-                              fs.writeFile(dir + fileName, buffer, function(){
-                                callback()
-                              })
-                          })
-                          .catch(function(e){
-                              console.log(e)
-                              callback()
-                          })
-                    })
-                }
-            index++
-            if (index >= count){
-                clearInterval(interval);
-            }
-          }, 2100);
-        });
-}
-
-function read_message_store_voicemail_limit(){
-    platform.get('/account/~/extension/~/message-store', {
-             dateFrom: '2018-01-01T00:00:00.000Z',
-             dateTo: '2018-12-31T23:59:59.999Z',
-             messageType: 'VoiceMail'
-        })
-        .then(function (resp) {
-          var jsonObj = resp.json()
-          var count = jsonObj.records.length
-          var index = 0
-          var dir = "voicemail_content/"
-          if(!fs.existsSync(dir)){
-            fs.mkdirSync(dir)
-          }
-          dir = "./" + dir
-          // Limit API call to ~40 calls per minute to avoid exceeding API rate limit.
-          var interval = setInterval(function() {
-            var record = jsonObj.records[index]
-            if (record.hasOwnProperty('attachments')){
-              async.each(record.attachments,
-                function(attachment, callback){
-                  var fileName = record.attachments[0].id + "_voicemail"
-                  if (attachment.type == "AudioRecording"){
-                      if (attachment.contentType == "audio/mpeg")
-                          fileName += ".mp3"
-                      else
-                          fileName += ".wav"
-                  }else if (attachment.type == "AudioTranscription" &&
-                            record.vmTranscriptionStatus == "Completed"){
-                      fileName += ".txt"
-                  }
-                  platform.get(attachment.uri)
-                      .then(function(res) {
-                          return res.response().buffer();
-                      })
-                      .then(function(buffer) {
-                          fs.writeFile(dir + fileName, buffer, function(){
-                            callback()
-                          })
-                      })
-                      .catch(function(e){
-                          console.log(e)
-                          callback()
-                      })
-                })
-            }
-            index++
-            if (index >= count){
-                clearInterval(interval);
-            }
-          }, 2100);
-        });
-
-}
-
 function read_message_store_message_content(){
     platform.get('/account/~/extension/~/message-store', {
              dateFrom: '2018-01-01T00:00:00.000Z',
-             dateTo: '2018-12-31T23:59:59.999Z'
+             dateTo: '2018-12-31T23:59:59.999Z',
         })
         .then(function (resp) {
           var jsonObj = resp.json()
           var count = jsonObj.records.length
           var index = 0
-          var contentPath = "content/"
+          var contentPath = "./content/"
           if(!fs.existsSync(contentPath)){
             fs.mkdirSync(contentPath)
           }
-          contentPath = "./" + contentPath
           // Limit API call to ~40 calls per minute to avoid exceeding API rate limit.
           var interval = setInterval(function() {
             var record = jsonObj.records[index]
@@ -199,22 +45,20 @@ function read_message_store_message_content(){
                 function(attachment, callback){
                   var fileName = ""
                   if (record.type == "VoiceMail"){
+                      var fileExt = getFileExteensionFromMimeType(attachment.contentType)
                       if (attachment.type == "AudioRecording"){
-                          if (attachment.contentType == "audio/mpeg")
-                              fileName = "voicemail_recording_"+record.attachments[0].id+".mp3"
-                          else
-                              fileName = "voicemail_recording_"+record.attachments[0].id+".wav"
+                        fileName = "voicemail_recording_" + record.attachments[0].id + fileExt
                       }else if (attachment.type == "AudioTranscription" &&
                                 record.vmTranscriptionStatus == "Completed"){
                           fileName = "voicemail_transcript_"+record.attachments[0].id+".txt"
                       }
                   }else if (record.type == "Fax"){
-                      var fileNameExt = attachment.contentType.split("/")
-                      fileName = "fax_attachment_" + attachment.id + "." + fileNameExt[1]
+                      var fileExt = getFileExteensionFromMimeType(attachment.contentType)
+                      fileName = "fax_attachment_" + attachment.id + fileExt
                   }else if (record.type == "SMS"){
                       if (attachment.type == "MmsAttachment"){
-                          var fileNameExt = attachment.contentType.split("/")
-                          fileName = "mms_attachment_" + record.attachments[0].id + "." + fileNameExt[1]
+                          var fileExt = getFileExteensionFromMimeType(attachment.contentType)
+                          fileName = "mms_attachment_" + record.attachments[0].id + fileExt
                       }else{
                           fileName = "sms_text_" + record.attachments[0].id + ".txt"
                       }
@@ -240,5 +84,38 @@ function read_message_store_message_content(){
             }
           }, 2100);
         });
+}
 
+function getFileExteensionFromMimeType(mimeType){
+  switch (mimeType){
+    case "text/html": return "html"
+    case "text/css": return ".css"
+    case "text/xml": return ".xml"
+    case "text/plain": return ".txt"
+    case "text/x-vcard": return ".vcf"
+    case "application/msword": return ".doc"
+    case "application/pdf": return ".pdf"
+    case "application/rtf": return ".rtf"
+    case "application/vnd.ms-excel": return ".xls"
+    case "application/vnd.ms-powerpoint": return ".ppt"
+    case "application/zip": return ".zip"
+    case "image/tiff": return ".tiff"
+    case "image/gif": return ".gif"
+    case "image/jpeg": return ".jpg"
+    case "image/png": return ".png"
+    case "image/x-ms-bmp": return ".bmp"
+    case "image/svg+xml": return ".svg"
+    case "audio/wav":
+    case "audio/x-wav":
+      return ".wav"
+    case "audio/mpeg": return ".mp3"
+    case "audio/ogg": return ".ogg"
+    case "video/3gpp": return ".3gp"
+    case "video/mpeg": return ".mpeg"
+    case "video/quicktime": return ".mov"
+    case "video/x-flv": return ".flv"
+    case "video/x-ms-wmv": return ".wmv"
+    case "video/mp4": return ".mp4"
+    default: return ".unknown"
+  }
 }
